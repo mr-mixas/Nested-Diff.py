ad9d81856a2d058e623cff0315efe74392142df1
Michael Samoglyadov <mixas.sr@gmail.com>
2018-12-23 22:00:24 +0300

    diff and patch for tuples implemented

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -2,3 +2,10 @@ from nested_diff import diff
 
 
 ## Test what doesn't covered by external (JSON based) tests
+
+def test_tuples_diff():
+    a = (1, 2, 4, 5)
+    b = (0, 1, 2, 3)
+
+    assert {'D': ({'A': 0}, {'I': 2, 'N': 3}, {'R': 5})} == \
+        diff(a, b, O=False, U=False)
