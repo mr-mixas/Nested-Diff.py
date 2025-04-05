defc08dea59aac85ace23e60d6d197c9cacc0ef6
Michael Samoglyadov <mixas.sr@gmail.com>
2025-04-05 10:00:44 +0300

    v1.7.1

--- nested_diff/__init__.py
+++ nested_diff/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2018-2024 Michael Samoglyadov
+# Copyright 2018-2025 Michael Samoglyadov
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ import nested_diff.handlers
 
 __all__ = ['Differ', 'Iterator', 'Patcher', 'diff', 'patch']
 
-__version__ = '1.7.0'
+__version__ = '1.7.1'
 __author__ = 'Michael Samoglyadov'
 __license__ = 'Apache License, Version 2.0'
 __website__ = 'https://github.com/mr-mixas/Nested-Diff.py'
